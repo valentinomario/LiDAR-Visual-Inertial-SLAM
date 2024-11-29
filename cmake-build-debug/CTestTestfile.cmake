ea824b8048d19b456f3fbbebb807c257d99d2e9c
Mario <valentinomario05@gmail.com>
2024-11-29 10:34:46 +0100

    initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/ros2_ws/src/emv-lio2
+# Build directory: /home/user/ros2_ws/src/emv-lio2/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
