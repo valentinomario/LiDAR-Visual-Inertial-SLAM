33bde5c5db2d0e13f312329f7880ce2a79090722
Mario <valentinomario05@gmail.com>
2024-12-01 11:43:38 +0100

    VF Lidar Callback

--- src/visual_odometry/visual_feature/feature_tracker_node.cpp
+++ src/visual_odometry/visual_feature/feature_tracker_node.cpp
@@ -36,6 +36,9 @@ public:
         // std::string pkg_path = ament_index_cpp::get_package_share_directory(PROJECT_NAME);
         auto config_path = this->declare_parameter<std::string>("config_dir", "/home/user/ros2_ws/install/emv_lio2/share/emv_lio2/config");
 
+        tf_buffer = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+        tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+
         readParameters(config_path);
     }
 
@@ -89,24 +92,110 @@ public:
 
 private:
 
-    /*void callback_two(const std::shared_ptr<sensor_msgs::msg::Image>& img0,
-        const std::shared_ptr<sensor_msgs::msg::Image>& img1)
-    {
-        RCLCPP_INFO(this->get_logger(), "Image callback works TODO remove this log");
-
-
-    }*/
 
     void callback_two(const sensor_msgs::msg::Image& img0, const sensor_msgs::msg::Image& img1)
     {
-        RCLCPP_INFO(this->get_logger(), "Image callback works TODO remove this log");
         // https://stackoverflow.com/questions/76568459/ros2-synchronizer-register-callback-inside-a-class-problem
+
+        static int i = 0;
+        RCLCPP_INFO(this->get_logger(), "Lidar callback works TODO remove this log %d", i);
+        i++;
 
     }
 
     void lidar_callback(const sensor_msgs::msg::PointCloud2& laser_msg)
     {
-        RCLCPP_INFO(this->get_logger(), "Lidar callback works TODO remove this log");
+        static int lidar_count = -1;
+        if (++lidar_count % (LIDAR_SKIP+1) != 0)
+            return;
+
+        // 0. listen to transform
+
+        geometry_msgs::msg::TransformStamped transformStamped;
+        try
+        {
+            transformStamped = tf_buffer->lookupTransform(
+                       "vins_world", "vins_body_ros", laser_msg.header.stamp, tf2::durationFromSec(0.01));
+        }
+        catch (tf2::TransformException ex){
+            // ROS_ERROR("lidar no tf");
+            RCLCPP_WARN(this->get_logger(), "Could not get transform: %s", ex.what());
+            return;
+        }
+
+        Eigen::Affine3f transNow = tf2::transformToEigen(transformStamped).cast<float>();
+
+
+        // 1. convert laser cloud message to pcl，此时laser_cloud_in是pcl格式的/hesai/pandar点云(360°的)
+        pcl::PointCloud<PointType>::Ptr laser_cloud_in(new pcl::PointCloud<PointType>());
+        pcl::fromROSMsg(laser_msg, *laser_cloud_in);
+
+        // 2. downsample current cloud (save memory)
+        pcl::PointCloud<PointType>::Ptr laser_cloud_in_ds(new pcl::PointCloud<PointType>());
+
+        down_size_random_sample_filter.setInputCloud(laser_cloud_in);
+        down_size_random_sample_filter.setSample(laser_cloud_in->size() * 0.3);
+        down_size_random_sample_filter.filter(*laser_cloud_in_ds);
+
+        *laser_cloud_in = *laser_cloud_in_ds;
+
+
+        // TODO: transform to IMU body frame
+        // 4. offset T_lidar -> T_camera
+        pcl::PointCloud<PointType>::Ptr laser_cloud_offset(new pcl::PointCloud<PointType>());
+        Eigen::Affine3f transOffset = pcl::getTransformation(0,
+                                                             0,
+                                                             0,
+                                                             0,
+                                                             0,
+                                                             0);  // lidar to vins_body_ros
+
+        pcl::transformPointCloud(*laser_cloud_in, *laser_cloud_offset, transOffset);
+        *laser_cloud_in = *laser_cloud_offset;
+
+        // 5. transform new cloud into global odom frame
+        pcl::PointCloud<PointType>::Ptr laser_cloud_global(new pcl::PointCloud<PointType>());
+        pcl::transformPointCloud(*laser_cloud_in, *laser_cloud_global, transNow);
+
+        // 6. save new cloud
+        double timeScanCur = rclcpp::Time(laser_msg.header.stamp).seconds();
+        cloudQueue.push_back(*laser_cloud_global);
+        timeQueue.push_back(timeScanCur);
+
+        // 7. pop old cloud
+        while (!timeQueue.empty())
+        {
+            if (timeScanCur - timeQueue.front() > 2.0)
+            {
+                cloudQueue.pop_front();
+                timeQueue.pop_front();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        std::lock_guard<std::mutex> lock(mtx_lidar);
+        // 8. fuse global cloud
+        depthCloud->clear();
+        for (int i = 0; i < static_cast<int>(cloudQueue.size()); ++i)
+        {
+            *depthCloud += cloudQueue[i];
+        }
+
+
+        // 9. downsample global cloud
+        pcl::PointCloud<PointType>::Ptr depthCloudDS(new pcl::PointCloud<PointType>());
+        down_size_random_sample_filter.setInputCloud(depthCloud);
+        down_size_random_sample_filter.setSample(depthCloud->size() * 0.3);
+        down_size_random_sample_filter.filter(*depthCloudDS);
+
+        *depthCloud = *depthCloudDS;
+
+        static int lidar_cnt = 0;
+        RCLCPP_INFO(this->get_logger(), "Lidar callback works TODO remove this log %d", lidar_cnt);
+        lidar_cnt++;
 
     }
 
@@ -124,6 +213,11 @@ private:
     std::shared_ptr<message_filters::Synchronizer<ImgSyncPolicy>> sync;
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_lidar;
+
+
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener;
+    pcl::RandomSample<PointType> down_size_random_sample_filter;
 
     bool nodeInitialized = false;
 
